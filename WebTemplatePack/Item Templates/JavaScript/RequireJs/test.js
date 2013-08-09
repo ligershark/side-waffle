f041c6519e530f18ac9aac16e2cc1eacc7916844
Mads Kristensen <post@madskristensen.net>
2013-08-09 08:40:08 -0700

    Initial commit

---
+++ WebTemplatePack/Item Templates/JavaScript/RequireJs/test.js
@@ -0,0 +1,6 @@
+﻿require(["helper/util"], function (util) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+});
