64dd62292cd47b73b5da8827b5872971511f648c
John Papa <john@johnpapa.net>
2013-08-25 23:23:39 -0400

    Adding Controller, Directive and Factory item templates for AngularJs.

---
+++ WebTemplatePack/ItemTemplates/JavaScript/AngularJs/Factory/factory.js
@@ -0,0 +1,34 @@
+﻿/*
+* John Papa
+* http://johnpapa.net
+* AngularJS Factory File
+**/
+
+(function () {
+    'use strict';
+
+    // Factory name is handy inside of a factory for logging
+    var serviceId = '$safeitemname$Service';
+
+    // Define the factory on the module.
+    // Inject the dependencies. 
+    // Point to the factory definition function.
+    angular.module('app').factory(serviceId, ['config', $safeitemname$Service]);
+
+    function $safeitemname$Service(config) {
+        // Define the functions and properties to reveal.
+        var service = {
+            getData: getData
+        };
+
+        return service;
+
+        function getData() {
+
+        }
+
+        //#region Internal Methods        
+
+        //#endregion
+    }
+})();
