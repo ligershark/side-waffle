17dfe78259b5e55655deba858ed1e5ec186854d7
Mads Kristensen <post@madskristensen.net>
2013-08-25 21:23:12 -0700

    More Angular templates

--- WebTemplatePack/ItemTemplates/JavaScript/AngularJs/Controller/controller.js
+++ WebTemplatePack/ItemTemplates/JavaScript/AngularJs/Controller/controller.js
@@ -7,15 +7,15 @@
     'use strict';
 
     // Controller name is handy inside of a controller for logging
-    var controllerId = '$safeitemname$Controller';
+    var controllerId = '$safeitemname$';
 
     // Define the controller on the module.
     // Inject the dependencies. 
     // Point to the controller definition function.
     angular.module('app').controller(controllerId,
-        ['$scope', $safeitemname$Controller]);
+        ['$scope', $safeitemname$]);
 
-    function $safeitemname$Controller($scope) {
+    function $safeitemname$($scope) {
         // Using 'Controller As' syntax, so we assign this to the vm variable (for viewmodel).
         var vm = this;
 
---
+++ WebTemplatePack/ItemTemplates/JavaScript/AngularJs/Module/module.js
@@ -0,0 +1,30 @@
+﻿/*
+* John Papa
+* http://johnpapa.net
+* AngularJS App Module File
+**/
+
+(function () {
+    'use strict';
+
+    // Module name is handy for logging
+    var id = '$safeitemname$';
+
+    // Create the module and define its dependencies.
+    var $safeitemname$ = angular.module('$safeitemname$', [
+        // Angular modules 
+        'ngAnimate',        // animations
+        'ngRoute'           // routing
+
+        // Custom modules 
+
+        // 3rd Party Modules
+        
+    ]);
+
+    // Execute bootstrapping code and any dependencies.
+    $safeitemname$.run(['$q', '$rootScope',
+        function ($q, $rootScope) {
+
+        }]);
+})();
