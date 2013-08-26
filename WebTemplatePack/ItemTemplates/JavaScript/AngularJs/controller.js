b81299c39456c0437adddee8bffc5ee08b6576be
John Papa <john@johnpapa.net>
2013-08-25 22:53:09 -0400

    Adding AngularJS Controller File

---
+++ WebTemplatePack/ItemTemplates/JavaScript/AngularJs/controller.js
@@ -0,0 +1,32 @@
+﻿/*
+* John Papa
+* http://johnpapa.net
+* AngularJS Controller File
+**/
+(function () {
+    'use strict';
+
+    // Controller name is handy inside of a controller for logging
+    var controllerId = 'customController';
+
+    // Define the controller on the module.
+    // Inject the dependencies. 
+    // Point to the controller definition function.
+    angular.module('app').controller(controllerId,
+        ['$scope', customController]);
+
+    function customController($scope) {
+        // Using 'Controller As' syntax, so we assign this to the vm variable (for viewmodel).
+        var vm = this;
+
+        // Bindable properties and functions are plaed on vm.
+        vm.activate = activate;
+
+        function activate() {
+        }
+
+        //#region Internal Methods        
+
+        //#endregion
+    }
+})();
