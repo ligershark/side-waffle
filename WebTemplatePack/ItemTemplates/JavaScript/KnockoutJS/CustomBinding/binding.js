900e155226494f98dac23871d6d2bf76f8c18af7
mlorbe@microsoft.com <mlorbe@microsoft.com>
2013-09-02 21:47:45 -0700

    jQuery plugin and KnockoutJs Custom Binding templates

---
+++ WebTemplatePack/ItemTemplates/JavaScript/KnockoutJS/CustomBinding/binding.js
@@ -0,0 +1,12 @@
+﻿//Template taken from http://knockoutjs.com/documentation/custom-bindings.html
+ko.bindingHandlers['$safeitemname$'] = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        // This will be called when the binding is first applied to an element
+        // Set up any initial state, event handlers, etc. here
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        // This will be called once when the binding is first applied to an element,
+        // and again whenever the associated observable changes value.
+        // Update the DOM element based on the supplied values here.
+    }
+};
