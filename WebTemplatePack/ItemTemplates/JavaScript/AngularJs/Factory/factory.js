f3ce5f9d98741bc80a5c8dec51c0a0cbc6841b2e
John Papa <john@johnpapa.net>
2013-09-06 19:32:31 -0400

    Changed Controller As title for the template. Replaced config with $http for the Factory template (works out of the box now)

--- WebTemplatePack/ItemTemplates/JavaScript/AngularJs/Factory/factory.js
+++ WebTemplatePack/ItemTemplates/JavaScript/AngularJs/Factory/factory.js
@@ -7,9 +7,10 @@
     // Define the factory on the module.
     // Inject the dependencies. 
     // Point to the factory definition function.
-    angular.module('app').factory(serviceId, ['config', $safeitemname$]);
+    // TODO: replace app with your module name
+    angular.module('app').factory(serviceId, ['$http', $safeitemname$]);
 
-    function $safeitemname$(config) {
+    function $safeitemname$($http) {
         // Define the functions and properties to reveal.
         var service = {
             getData: getData
