460aca078bf913b4be887a8241d538fa635fb790
Mads Kristensen <post@madskristensen.net>
2013-09-25 19:55:39 -0700

    Adding the first project template

---
+++ WebTemplatePack/ProjectTemplates/Extensibility/BrowserLinkExtension/MyExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.Web.BrowserLink;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace $safeprojectname$
+{
+    [Export(typeof(IBrowserLinkExtensionFactory))]
+    public class MyExtensionFactory : IBrowserLinkExtensionFactory
+    {
+        public BrowserLinkExtension CreateExtensionInstance(BrowserLinkConnection connection)
+        {
+            return new MyExtension();
+        }
+
+        public string GetScript()
+        {
+            using (Stream stream = GetType().Assembly.GetManifestResourceStream("$safeprojectname$.Scripts.$safeprojectname$.js"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+
+    public class MyExtension : BrowserLinkExtension
+    {
+        public override void OnConnected(BrowserLinkConnection connection)
+        {
+            Browsers.Client(connection).Invoke("greeting", "Hello from Visual Studio!");
+        }
+
+        [BrowserLinkCallback] // This method can be called from JavaScript
+        public void SendText(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
