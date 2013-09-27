0635197d8586e1547704b22723cad9fea7f06355
Mads Kristensen <post@madskristensen.net>
2013-09-27 10:11:50 -0700

    Added Browser Link item template

---
+++ WebTemplatePack/ItemTemplates/Extensibility/BrowserLink/MyExtension.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../intellisense/browserlink.intellisense.js" />
+
+(function (browserLink, $) {
+    /// <param name="browserLink" value="bl" />
+    /// <param name="$" value="jQuery" />
+
+    function output(message) { // Helper for the 'greeting' function
+        alert(message);
+
+        if (console) {
+            console.log(message);
+        }
+    }
+
+    return {
+
+        greeting: function (message) { // Can be called from the server-side extension
+            output(message);
+        },
+
+        onConnected: function () { // Optional. Is called when a connection is established
+            browserLink.invoke("SendText", "Hello from " + browserLink.initializationData.appName);
+        }
+    };
+});
