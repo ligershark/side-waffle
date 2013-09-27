2722d32defb798e737d5698ecc3805161fe1d498
Mads Kristensen <post@madskristensen.net>
2013-09-27 16:13:26 -0700

    Added .vsct item template

---
+++ WebTemplatePack/ItemTemplates/Extensibility/VS Command Table/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace $rootnamespace$
+{
+    static class GuidList
+    {
+        public const string guidMyPackagePkgString = "$guid1$";
+        public const string guidMyPackageCmdSetString = "$guid2$";
+
+        public static readonly Guid guidMyPackageCmdSet = new Guid(guidMyPackageCmdSetString);
+    };
+}
