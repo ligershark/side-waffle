e6aeb2bf11e3df47c235cb1a19e6a8fcc2bb1f19
bradygaster <brady.gaster@gmail.com>
2013-09-27 23:22:27 -0700

    added signalr hub and page consumer template

---
+++ WebTemplatePack/ItemTemplates/Web/SignalR/BasicHubAndPage/HitCounterHub.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * In the Application_Start method, or somewhere in app's start
+ * you'll need to add the namespace inclusion:
+ * 
+ * using Microsoft.AspNet.SignalR;
+ * 
+ * and the code:
+ * 
+ * RouteTable.Routes.MapHubs();
+ * 
+ * */
+
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace $rootnamespace$
+{
+    [HubName("myHub")]
+    public class $safeitemname$ : Hub
+    {
+        static int _hitCount;
+
+        public void RecordHit()
+        {
+            _hitCount += 1;
+            base.Clients.All.onHitCountUpdated(_hitCount);
+        }
+    }
+}
