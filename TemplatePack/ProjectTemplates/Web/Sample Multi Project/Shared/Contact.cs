34af623bb19873d9f4b6dd114c0a646ac1af0971
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-10-05 19:31:37 -0700

    basic multi project sample added

---
+++ TemplatePack/ProjectTemplates/Web/Sample Multi Project/Shared/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    public class Contact
+    {
+        public long Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ TemplatePack/ProjectTemplates/Web/Sample Multi Project/Web/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using My.Cool.Company.SharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace $safeprojectname$ {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            Contact contact = new Contact {
+                Firstname = "Sayed",
+                Lastname = "Hashimi"
+            };
+        }
+    }
+}
