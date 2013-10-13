598893354938b763965118b30e382af2cf715ccc
Vitali Fokin <vitali.fokin@gmail.com>
2013-10-13 22:16:24 +0300

    add NUnit fixture item template

---
+++ TemplatePack/ItemTemplates/Test/NUnitFixture/NUnitFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace $rootnamespace$
+{
+    [TestFixture]
+    public class $safeitemname$
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
