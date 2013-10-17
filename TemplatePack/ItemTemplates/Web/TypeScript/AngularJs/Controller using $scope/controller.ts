23ab745e8f5436cd7039928214635a2d87c148ce
James McLachlan <claquer@gmail.com>
2013-10-16 22:09:17 -0400

    Added controller using $scope

--- TemplatePack/ItemTemplates/Web/TypeScript/AngularJs/Controller using $scope/controller.ts
+++ TemplatePack/ItemTemplates/Web/TypeScript/AngularJs/Controller using $scope/controller.ts
@@ -1,27 +1,27 @@
-// Install the angularjs.TypeScript.DefinitelyTyped NuGet package to resovle the reference paths,
+﻿// Install the angularjs.TypeScript.DefinitelyTyped NuGet package to resovle the reference paths,
 // then adjust the path value to be relative to this file
 /// <reference path='../Scripts/typings/angularjs/angular.d.ts'/>
 /// <reference path='../Scripts/typings/angularjs/angular-resource.d.ts'/>
 
 interface I$safeitemname$Scope extends ng.IScope {
-    vm: $safeitemname$;
+    greeting: string;
+    changeGreeting: () => void;
 }
 
 interface I$safeitemname$ {
-    greeting: string;
     controllerId: string;
-    changeGreeting: () => void;
 }
 
 class $safeitemname$ implements I$safeitemname$ {
     static controllerId: string = "$safeitemname$";
-    greeting = "Hello";
     
     constructor(private $scope: I$safeitemname$Scope, private $http: ng.IHttpService, private $resource: ng.resource.IResourceService) {
+        $scope.greeting = "Hello";
+        $scope.changeGreeting = () => this.changeGreeting();
     }
 
-    changeGreeting() {
-        this.greeting = "Bye";
+    private changeGreeting() {
+        this.$scope.greeting = "Bye";
     }
 }
 
