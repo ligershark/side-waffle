26b3368d2a104b5e7b5c35d538ea02562a75d6b3
James McLachlan <claquer@gmail.com>
2013-10-18 16:39:15 -0400

    Factory work

---
+++ TemplatePack/ItemTemplates/Web/TypeScript/AngularJs/TypeScript Factory/factory.js
@@ -0,0 +1,24 @@
+﻿// Install the angularjs.TypeScript.DefinitelyTyped NuGet package to resovle the reference paths,
+// then adjust the path value to be relative to this file
+/// <reference path='../Scripts/typings/angularjs/angular.d.ts'/>
+/// <reference path='../Scripts/typings/angularjs/angular-resource.d.ts'/>
+var $safeitemname$ = (function () {
+    function $safeitemname$($http, $resource) {
+        this.$http = $http;
+        this.$resource = $resource;
+        this.greeting = "Hello";
+    }
+    $safeitemname$.prototype.changeGreeting = function () {
+        this.greeting = "Bye";
+    };
+    $safeitemname$.serviceId = "$safeitemname$";
+    return $safeitemname$;
+})();
+
+app.factory($safeitemname$.serviceId, [
+    '$http',
+    '$resource',
+    function ($scope, $http, $resource) {
+        return new $safeitemname$($http, $resource);
+    }
+]);
---
+++ TemplatePack/ItemTemplates/Web/TypeScript/AngularJs/TypeScript Factory/factory.ts
@@ -0,0 +1,26 @@
+﻿// Install the angularjs.TypeScript.DefinitelyTyped NuGet package to resovle the reference paths,
+// then adjust the path value to be relative to this file
+/// <reference path='../Scripts/typings/angularjs/angular.d.ts'/>
+/// <reference path='../Scripts/typings/angularjs/angular-resource.d.ts'/>
+
+interface I$safeitemname$ {
+    greeting: string;
+    serviceId: string;
+    changeGreeting: () => void;
+}
+
+class $safeitemname$ implements I$safeitemname$ {
+    static serviceId: string = "$safeitemname$";
+    greeting = "Hello";
+
+    constructor(private $http: ng.IHttpService, private $resource: ng.resource.IResourceService) {
+    }
+
+    changeGreeting() {
+        this.greeting = "Bye";
+    }
+}
+
+app.factory($safeitemname$.serviceId, ['$http', '$resource', function ($scope, $http, $resource) {
+    return new $safeitemname$($http, $resource);
+}]);
