4b7e532876ce3ce63ceb003a48433e302f1df04a
James McLachlan <claquer@gmail.com>
2013-10-19 22:59:04 -0400

    Setting up for directive work

---
+++ TemplatePack/ItemTemplates/Web/TypeScript/AngularJs/TypeScript Directive/directive.ts
@@ -0,0 +1,24 @@
+﻿(function() {
+    'use strict';
+
+    // Define the directive on the module.
+    // Inject the dependencies. 
+    // Point to the directive definition function.
+    angular.module('app').directive('$safeitemname$', ['$window', $safeitemname$]);
+    
+    function $safeitemname$ ($window) {
+        // Usage:
+        // 
+        // Creates:
+        // 
+        var directive = {
+            link: link,
+            restrict: 'A'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+        }
+    }
+
+})();
