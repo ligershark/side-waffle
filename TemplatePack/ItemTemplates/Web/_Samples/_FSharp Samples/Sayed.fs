29361c6c1551244437fc7f7d6b632f41da049660
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-11-30 15:25:22 -0800

    adding basic support for F# templates. Depnds on templatebuider 1.0.3.22-beta. Related to #62

---
+++ TemplatePack/ItemTemplates/Web/_Samples/_FSharp Samples/Sayed.fs
@@ -0,0 +1,4 @@
+﻿module $safeitemname$
+
+// this is a sample F# file by Sayed (yes I know it's lame, I don't know much F#). 
+//  Please improve/replace it
---
+++ TemplatePack/ProjectTemplates/Web/_SampleFSharpTemplate/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+// custom template by Sayed
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
