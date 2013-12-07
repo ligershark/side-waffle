7f3e3050c4c0c95fd10e94afc636de3e463932ec
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-12-07 12:16:02 -0800

    Updating for custom wizard support. Related to #63

--- TemplatePack/ProjectTemplates/Web/_Sample Multi Project/Shared/Contact.cs
+++ TemplatePack/ProjectTemplates/Web/_Sample Multi Project/Shared/Contact.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// replace test Sayed
+// replace test $saferootprojectname$
 namespace $safeprojectname$
 {
     public class Contact
