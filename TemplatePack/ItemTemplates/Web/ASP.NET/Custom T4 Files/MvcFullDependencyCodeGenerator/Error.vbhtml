6e5c236e38d097c275f16082d4074f14ec2466e9
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-12-26 12:43:51 -0800

    Adding ASP.NET T4 custom scaffolder files. Resolving #69

---
+++ TemplatePack/ItemTemplates/Web/ASP.NET/Custom T4 Files/MvcFullDependencyCodeGenerator/Error.vbhtml
@@ -0,0 +1,20 @@
+﻿@ModelType System.Web.Mvc.HandleErrorInfo
+
+@Code
+    Layout = Nothing
+End Code
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>Error</title>
+</head>
+<body>
+    <hgroup>
+        <h1 class="text-danger">Error.</h1>
+        <h2 class="text-danger">An error occurred while processing your request.</h2>
+    </hgroup>
+</body>
+</html>
+
---
+++ TemplatePack/ItemTemplates/Web/ASP.NET/Custom T4 Files/update.ps1
@@ -0,0 +1,29 @@
+﻿$str = ""
+$csStr = ""
+$vbStr = ""
+
+Get-ChildItem *.t4 -Recurse | 
+    ForEach-Object { 
+        $relPath = ((Resolve-Path $_.FullName -Relative).Substring(2))
+
+        $isCsFile = ($relPath.EndsWith('.cs.t4'))
+        $isVbFile = ($relPath.EndsWith('.vb.t4'))
+
+        $strFragment = ("<ProjectItem TargetFileName=""CodeTemplates\$relPath"" ReplaceParameters=""true"">$relPath</ProjectItem>{1}" -f $relPath,[Environment]::NewLine)
+        if($relPath.EndsWith('.cs.t4')){
+            $csStr += $strFragment
+        }
+        if($relPath.EndsWith('.vb.t4')){
+            $vbStr += $strFragment
+        }
+    }
+
+    $str = ("******** CSharp ********{0}" -f [Environment]::NewLine)
+    $str += $csStr
+    
+    $str += ("{0}{0}******** Visual Basic ********{0}" -f [Environment]::NewLine)
+    $str += $vbStr
+
+    $str | Write-Host
+
+Set-Content C:\temp\scaffoldingtemp.txt -Value $str
