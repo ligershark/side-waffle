4be5ee61963691dab105d2ecaa471c1a2e1b80a7
Paul Betts <paul@github.com>
2014-01-05 23:12:52 -0800

    Initial drop of reactiveui/projecttemplates@d690aa0b

---
+++ TemplatePack/ProjectTemplates/Cross-Platform/ReactiveUI/ProjectTemplate.Core/ViewModels/ColorPickerViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using ReactiveUI;
+
+namespace ProjectTemplate.ViewModels
+{
+    public class ColorPickerViewModel : ReactiveObject
+    {
+        int red;
+        public int Red {
+            get { return red; }
+            set { this.RaiseAndSetIfChanged(ref red, value); }
+        }
+
+        int green;
+        public int Green {
+            get { return green; }
+            set { this.RaiseAndSetIfChanged(ref green, value); }
+        }
+
+        int blue;
+        public int Blue {
+            get { return blue; }
+            set { this.RaiseAndSetIfChanged(ref blue, value); }
+        }
+
+        ObservableAsPropertyHelper<Color?> finalColor;
+        public Color? FinalColor {
+            get { return finalColor.Value; }
+        }
+
+        public ColorPickerViewModel()
+        {
+            this.WhenAnyValue(x => x.Red, x => x.Green, x => x.Blue,
+                    (r, g, b) => new { Red = r, Green = g, Blue = blue})
+                .Where(rgb =>
+                    InRange(rgb.Red) && InRange(rgb.Green) && InRange(rgb.Blue))
+                .Select(rgb =>
+                    (Color?)Color.FromArgb(rgb.Red, rgb.Green, rgb.Blue))
+                .ToProperty(this, x => x.FinalColor, out finalColor, Color.Black);
+        }
+
+        bool InRange(int colorVal)
+        {
+            return colorVal >= 0 && colorVal <= 255;
+        }
+    }
+}
---
+++ TemplatePack/ProjectTemplates/Cross-Platform/ReactiveUI/ProjectTemplate.WPF/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ProjectTemplate.ViewModels;
+using ReactiveUI.Xaml;
+
+namespace ProjectTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foo = new ViewModelViewHost();
+            ViewHost.ViewModel = new ColorPickerViewModel();
+        }
+    }
+}
