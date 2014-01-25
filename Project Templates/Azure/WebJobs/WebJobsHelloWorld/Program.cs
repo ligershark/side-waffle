167a4a729582f3368fa8e61108ba84f943f31d98
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-01-25 12:34:18 -0800

    updating to 1.0.3.44-beta templatebuilder. Resolved #88 Resolved #87 Related to #89

---
+++ Project Templates/Azure/WebJobs/WebJobsHelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.WindowsAzure.Jobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJobsHelloWorld {
+    // To learn more about Windows Azure WebJobs start here http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program {
+        // Please set the following connectionstring values in app.config
+        // AzureJobsRuntime and AzureJobsData
+        static void Main() {
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        public static void ProcessQueueMessage([QueueInput] string inputText, [BlobOutput("containername/blobname")]TextWriter writer) {
+            writer.WriteLine(inputText);
+        }
+    }
+}
