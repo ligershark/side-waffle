b2e3b8f123d0b846aa0e5ac777f07c34f48c5714
John Papa <john@johnpapa.net>
2014-02-10 20:43:59 -0500

    fixed spelling errors in TS files

--- TemplatePack/ItemTemplates/TypeScript/AngularJs/TypeScript Controller using $scope/controller.ts
+++ TemplatePack/ItemTemplates/TypeScript/AngularJs/TypeScript Controller using $scope/controller.ts
@@ -1,5 +1,5 @@
 ﻿// Update the reference to app1.ts to be that of your module file.
-// Install the angularjs.TypeScript.DefinitelyTyped NuGet package to resovle the .d.ts reference paths,
+// Install the angularjs.TypeScript.DefinitelyTyped NuGet package to resolve the .d.ts reference paths,
 // then adjust the path value to be relative to this file
 /// <reference path="app1.ts" />
 /// <reference path='/Scripts/typings/angularjs/angular.d.ts'/>
