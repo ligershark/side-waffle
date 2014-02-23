c0c3043bcd93fb27ef5cb6725f0e3cbe2f34033a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-02-23 12:27:35 -0800

    start custom wizard sample

---
+++ SideWaffle/SampleWizard.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideWaffle {
+    public class SampleWizard : IWizard {
+        // Use to communicate $saferootprojectname$ to ChildWizard     
+        public static Dictionary<string, string> GlobalDictionary = new Dictionary<string, string>();
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams) {
+            // Place "$saferootprojectname$ in the global dictionary. 
+            // Copy from $safeprojectname$ passed in my root vstemplate         
+            GlobalDictionary["$saferootprojectname$"] = replacementsDictionary["$safeprojectname$"];
+            GlobalDictionary["$datetime$"] = DateTime.Now.ToShortDateString();
+        }
+
+        public void BeforeOpeningFile(EnvDTE.ProjectItem projectItem) {
+        }
+
+        public void ProjectFinishedGenerating(EnvDTE.Project project) {
+        }
+
+        public void ProjectItemFinishedGenerating(EnvDTE.ProjectItem projectItem) {
+        }
+
+        public void RunFinished() {
+        }
+
+        
+
+        public bool ShouldAddProjectItem(string filePath) {
+            return true;
+        }
+    }
+}
