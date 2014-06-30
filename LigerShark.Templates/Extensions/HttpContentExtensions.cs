4ee58f34342e0068b048e25a6745687daf0102e4
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-06-30 01:14:15 -0700

    start htmp5up support. work in progress. related to #131

---
+++ LigerShark.Templates/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace LigerShark.Templates.Extensions {
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public static class HttpContentExtensions {
+        public static async Task ReadAsFileAsync(this HttpContent content, string filename, bool overwrite) {
+            string pathname = Path.GetFullPath(filename);
+            if (!overwrite && File.Exists(filename)) {
+                throw new InvalidOperationException(string.Format("File {0} already exists.", pathname));
+            }
+
+            using (var fileStream = new FileStream(pathname, FileMode.Create, FileAccess.Write, FileShare.None)) {
+                await content.CopyToAsync(fileStream);
+                fileStream.Close();
+            }
+        }
+    }
+}
---
+++ LigerShark.Templates/Html5UpProjectWizard.cs
@@ -0,0 +1,50 @@
+﻿namespace LigerShark.Templates {
+    using Microsoft.VisualStudio.TemplateWizard;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Html5UpProjectWizard : IWizard {
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams) {
+            // here we need to show the UI for which file to download
+            var form = new DownloadZipWindow();
+            var result = form.ShowDialog();
+
+            if (result.HasValue && result.Value) {
+                // download the file
+                string file = form.DownloadedFile;
+
+                string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+                if (!Directory.Exists(tempDir)) {
+                    Directory.CreateDirectory(tempDir);
+                }
+                
+                // unpack the file in temp
+                ZipFile.ExtractToDirectory(file, tempDir);
+
+                // copy the files to the project directory
+            }
+
+        }
+        public void BeforeOpeningFile(EnvDTE.ProjectItem projectItem) {
+        }
+
+        public void ProjectFinishedGenerating(EnvDTE.Project project) {
+        }
+
+        public void ProjectItemFinishedGenerating(EnvDTE.ProjectItem projectItem) {
+        }
+
+        public void RunFinished() {
+        }
+
+        public bool ShouldAddProjectItem(string filePath) {
+            return true;
+        }
+    }
+}
