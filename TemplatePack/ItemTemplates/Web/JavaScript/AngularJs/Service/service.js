cfcd705d3cfd365a77e93408b76480624e90ac9b
John Papa <john@johnpapa.net>
2014-09-03 00:14:57 -0400

    AngularJS Service and Test Spec

---
+++ TemplatePack/ItemTemplates/Web/JavaScript/AngularJs/Service/service.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .service('$safeitemname$', $safeitemname$);
+
+    $safeitemname$.$inject = ['$http'];
+
+    function $safeitemname$($http) {
+        this.getData = getData;
+
+        function getData() { }
+    }
+})();
---
+++ TemplatePack/ItemTemplates/Web/JavaScript/AngularJs/Test/spec.js
@@ -0,0 +1,18 @@
+﻿describe('$safeitemname$', function () {
+    var $controller,
+        $httpBackend,
+        $rootScope;
+
+    beforeEach(function () {
+        module('app');
+        inject(function (_$controller_, _$httpBackend_, _$rootScope_) {
+            $controller = _$controller_;
+            $httpBackend = _$httpBackend_;
+            $rootScope = _$rootScope_;
+        });
+    });
+
+    it('should exist', function () {
+        //TODO: expectation goes here
+    });
+});
