dd9ca801e914b00a3f6158f950a97f6a24c85b0a
Kent Cooper <kent@advantsystems.com>
2015-03-06 22:17:13 -0500

    Update TypeScript AngularJS Controller Templates to work with Angular 1.3
    Changed the controllers back to using class and constructor. Added
    static $inject field and and updated the function to set the controller.

--- TemplatePack/ItemTemplates/TypeScript/AngularJs/TypeScript Controller/controller.ts
+++ TemplatePack/ItemTemplates/TypeScript/AngularJs/TypeScript Controller/controller.ts
@@ -2,25 +2,30 @@
 // Install the angularjs.TypeScript.DefinitelyTyped NuGet package to resolve the .d.ts reference paths,
 // then adjust the path value to be relative to this file.
 /// <reference path="app1.ts" />
-/// <reference path='/Scripts/typings/angularjs/angular.d.ts'/>
-/// <reference path='/Scripts/typings/angularjs/angular-resource.d.ts'/>
+/// <reference path="/Scripts/typings/angularjs/angular.d.ts"/>
+/// <reference path="/Scripts/typings/angularjs/angular-resource.d.ts"/>
+
+interface I$safeitemname$Scope extends ng.IScope {
+    vm: $safeitemname$;
+}
 
 interface I$safeitemname$ {
     greeting: string;
     changeGreeting: () => void;
 }
 
-function $safeitemname$($scope: ng.IScope, $http: ng.IHttpService, $resource: ng.resource.IResourceService){
-    var vm: I$safeitemname$ = this;
-    vm.greeting = "Hello";
+class $safeitemname$ implements I$safeitemname$ {
+    static controllerId: string = "$safeitemname$";
+    static $inject: string[] = ["$scope", "$http", "$resource"];
+    greeting = "Hello";
 
-    vm.changeGreeting=(): void =>{
-        vm.greeting = "Bye";
+    constructor(private $scope: I$safeitemname$Scope, private $http: ng.IHttpService, private $resource: ng.resource.IResourceService) {
+    }
+
+    changeGreeting() {
+        this.greeting = "Bye";
     }
 }
 
-
 // Update the app1 variable name to be that of your module variable
-app1.controller("$safeitemname$", ['$scope', '$http', '$resource', 
-    $safeitemname$
-]);
+app1.controller($safeitemname$.controllerId, $safeitemname$);
