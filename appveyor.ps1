8967fe650eadb2b4db0c67ac0ee86abe9f4ea154
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-04-04 01:55:53 -0400

    updating build script to call appveyor.ps1

---
+++ appveyor.ps1
@@ -0,0 +1,11 @@
+﻿# install vsixgallery script
+(new-object Net.WebClient).DownloadString("https://raw.github.com/madskristensen/ExtensionScripts/master/AppVeyor/vsix.ps1") | iex
+
+Vsix-IncrementVsixVersion | Vsix-UpdateBuildVersion
+
+nuget restore sidewaffle.sln
+# install psbuild
+(new-object Net.WebClient).DownloadString("https://raw.github.com/ligershark/psbuild/master/src/GetPSBuild.ps1") | iex
+.\build-main.ps1
+
+Vsix-PushArtifacts | Vsix-PublishToGallery
