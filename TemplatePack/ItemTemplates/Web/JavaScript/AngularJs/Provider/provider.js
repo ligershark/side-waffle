19898b5c69751c85cd53176f0203b7830bcff5ce
Kent Cooper <kent@advantsystems.com>
2015-04-05 15:36:50 -0400

    Added AngularJS Provider Template
    Created a new ItemTemplate for an AngularJS Provider.

---
+++ TemplatePack/ItemTemplates/Web/JavaScript/AngularJs/Provider/provider.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .provider('$safeitemname$', $safeitemname$Provider);
+
+    function $safeitemname$Provider() {
+        var configValue = false;
+
+        this.setConfigValue = function (value) {
+            configValue = value;
+        };
+
+        this.$get = $safeitemname$Factory;
+
+        $safeitemname$Factory.$inject = ['$http'];
+        function $safeitemname$Factory($http) {
+            var service = {
+                getData: getData
+            };
+
+            return service;
+            
+
+            function getData() {                
+                return configValue;
+            }
+        }
+    }
+})();
