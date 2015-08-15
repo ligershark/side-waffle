2fe7384c6f41fe227f11cbe905cb6ecb70208a5c
Tyler Hughes <thughes3@jsu.edu>
2015-08-14 22:19:33 -0500

    Telemetry WIP

--- LigerShark.Templates/GoogleAnalyticsWizard.cs
+++ LigerShark.Templates/GoogleAnalyticsWizard.cs
@@ -9,33 +9,47 @@ namespace LigerShark.Templates
 {
     public class GoogleAnalyticsWizard : IWizard
     {
+        private object TemplateID { get; set; }
+        private object TemplateName { get; set; }
+
         public void BeforeOpeningFile(ProjectItem projectItem)
         {
+
         }
+
         // After the project template is created, send template ID to Google
         public void ProjectFinishedGenerating(Project project)
         {
-            sendTrackingCode("$TemplateID", "$TemplateName");
+            sendTrackingCode(TemplateID.ToString(), TemplateName.ToString());
         }
+
         // After the item template is created, send template ID to Google
         public void ProjectItemFinishedGenerating(ProjectItem projectItem)
         {
-            sendTrackingCode("$TemplateID", "$TemplateName");
+            sendTrackingCode(TemplateID.ToString(), TemplateName.ToString());
         }
+
         public void RunFinished()
         {
+
         }
+
         public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
+
+            TemplateName = customParams.GetValue(0);
+            //TemplateID = customParams.GetValue(1);
         }
+
         public bool ShouldAddProjectItem(string filePath)
         {
             return true;
         }
+
         private void sendTrackingCode(string templateID, string templateName)
         {
-            Console.WriteLine("Template Name: " + templateName);
-            Console.WriteLine("Template ID: " + templateID);
+            System.Windows.MessageBox.Show("Template Name: " + templateName);
+            System.Windows.MessageBox.Show("Template ID: " + templateID);
         }
     }
 }
