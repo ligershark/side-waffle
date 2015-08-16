349ae384f4c8f2f5b35422c336bfe6f8552a9177
Tyler Hughes <thughes3@jsu.edu>
2015-08-16 14:46:15 -0500

    WIP for telemetry with Google Analytics

--- LigerShark.Templates/GoogleAnalyticsWizard.cs
+++ LigerShark.Templates/GoogleAnalyticsWizard.cs
@@ -1,6 +1,14 @@
 ﻿using EnvDTE;
 using Microsoft.VisualStudio.TemplateWizard;
 using System.Collections.Generic;
+<<<<<<< HEAD
+=======
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+>>>>>>> WIP for telemetry with Google Analytics
 
 namespace LigerShark.Templates
 {
@@ -33,6 +41,10 @@ namespace LigerShark.Templates
 
         public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
+<<<<<<< HEAD
+=======
+
+>>>>>>> WIP for telemetry with Google Analytics
             TemplateName = replacementsDictionary["$TemplateName$"];
             TemplateID = replacementsDictionary["$TemplateID$"];
         }
@@ -44,8 +56,52 @@ namespace LigerShark.Templates
 
         private void TrackTemplate(string templateID, string templateName)
         {
+<<<<<<< HEAD
             GoogleAnalyticsApi tracker = new GoogleAnalyticsApi("UA-62483606-3", "1092810121650");
             tracker.TrackEvent("template", "add", templateName);
+=======
+            var request = (HttpWebRequest)WebRequest.Create("http://www.google-analytics.com/collect");
+            request.Method = "POST";
+
+            // the request body we want to send
+            var postData = new Dictionary<string, string>
+                           {
+                               { "v", "1" },
+                               { "tid", "UA-62483606-3" },
+                               { "cid", "555" },
+                               { "t", "event" },
+                               { "ec", "template" },
+                               { "ea", "add" },
+                               { "el", templateName },
+                               { "ev", templateID }
+                           };
+            var postDataString = postData
+                .Aggregate("", (data, next) => string.Format("{0}&{1}={2}", data, next.Key, HttpUtility.UrlEncode(next.Value)))
+                .TrimEnd('&');
+
+            request.ContentLength = Encoding.UTF8.GetByteCount(postDataString);
+
+            using (var writer = new StreamWriter(request.GetRequestStream()))
+            {
+                writer.Write(postDataString);
+            }
+
+            try
+            {
+                var webResponse = (HttpWebResponse)request.GetResponse();
+                if (webResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new HttpException((int)webResponse.StatusCode,
+                                            "Google Analytics tracking did not return OK 200");
+                }
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                System.Windows.MessageBox.Show("Failed to send data to Google Analytics\n" + ex.Message);
+
+            }
+>>>>>>> WIP for telemetry with Google Analytics
         }
     }
 }
