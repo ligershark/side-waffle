e69de14c2c191e9482902712534e3c70fc7b1756
Tyler Hughes <thughes3@jsu.edu>
2015-09-17 21:55:58 -0500

    WIP - adding implementation to upload data to GA

--- LigerShark.Templates/GoogleAnalyticsWizard.cs
+++ LigerShark.Templates/GoogleAnalyticsWizard.cs
@@ -1,12 +1,6 @@
 ﻿using EnvDTE;
 using Microsoft.VisualStudio.TemplateWizard;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Web;
 
 namespace LigerShark.Templates
 {
@@ -51,47 +45,7 @@ namespace LigerShark.Templates
 
         private void TrackTemplate(string templateID, string templateName)
         {
-            var request = (HttpWebRequest)WebRequest.Create("http://www.google-analytics.com/collect");
-            request.Method = "POST";
-
-            // the request body we want to send
-            var postData = new Dictionary<string, string>
-                           {
-                               { "v", "1" },
-                               { "tid", "UA-62483606-3" },
-                               { "cid", "555" },
-                               { "t", "event" },
-                               { "ec", "template" },
-                               { "ea", "add" },
-                               { "el", templateName },
-                               { "ev", templateID }
-                           };
-            var postDataString = postData
-                .Aggregate("", (data, next) => string.Format("{0}&{1}={2}", data, next.Key, HttpUtility.UrlEncode(next.Value)))
-                .TrimEnd('&');
-
-            request.ContentLength = Encoding.UTF8.GetByteCount(postDataString);
-
-            using (var writer = new StreamWriter(request.GetRequestStream()))
-            {
-                writer.Write(postDataString);
-            }
-
-            try
-            {
-                var webResponse = (HttpWebResponse)request.GetResponse();
-                if (webResponse.StatusCode != HttpStatusCode.OK)
-                {
-                    throw new HttpException((int)webResponse.StatusCode,
-                                            "Google Analytics tracking did not return OK 200");
-                }
-            }
-            catch (Exception ex)
-            {
-                // log the exception
-                System.Windows.MessageBox.Show("Failed to send data to Google Analytics\n" + ex.Message);
-
-            }
+            GoogleAnalyticsApi.TrackEvent("template", "add", templateName + templateID);
         }
     }
 }
