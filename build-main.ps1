0da8138852b8063b1071eac5b279bfb8aad4c4c3
RandomlyKnighted <thughes@randomlyknighted.com>
2016-03-22 10:55:23 -0500

    Removed write-host line

--- build-main.ps1
+++ build-main.ps1
@@ -563,8 +563,6 @@ function GetTemplateFiles{
                 throw ('Did not find templates folder at {0}' -f $f.FullName)
             }
 
-			Write-Host($f.FullName)
-
             $templatefiles += Get-ChildItem -Path $f.FullName -Filter _project.vstemplate.xml -Recurse -File | Where-Object { $_.Directory.Name -ne 'SW-ProjectVSTemplateFile' }
 
             $templatefiles += (Get-ChildItem -Path $f.FullName -Filter *.vstemplate -Recurse -File)
