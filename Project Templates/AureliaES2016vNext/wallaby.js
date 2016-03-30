cc5fa731f99abdff3d4f6aeadc0fa38338b23207
Tyler Hughes <thughes3@jsu.edu>
2016-03-29 23:29:04 -0500

    Added ASP.NET ES2016 template of Aurelia version 1.0.0-beta.1.2.1

---
+++ Project Templates/AureliaES2016vNext/wallaby.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-var, no-shadow, dot-notation */
+
+var babel = require('babel');
+
+module.exports = function(wallaby) {
+  return {
+    files: [
+
+      {pattern: 'wwwroot/jspm_packages/system.js', instrument: false},
+      {pattern: 'wwwroot/config.js', instrument: false},
+
+      {pattern: 'src/**/*.js', load: false},
+      {pattern: 'test/unit/setup.js', load: false}
+
+    ],
+
+    tests: [
+      {pattern: 'test/unit/**/*.spec.js', load: false}
+    ],
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel({
+        babel: babel,
+        optional: [
+          'es7.decorators',
+          'es7.classProperties'
+        ]
+      })
+    },
+
+    middleware: (app, express) => {
+      app.use('/wwwroot/jspm_packages', express.static(require('path').join(__dirname, 'wwwroot', 'jspm_packages')));
+      app.use('/jspm_packages', express.static(require('path').join(__dirname, 'wwwroot', 'jspm_packages')));
+    },
+
+    bootstrap: function(wallaby) {
+      var promises = [];
+      var i = 0;
+      var len = wallaby.tests.length;
+
+      wallaby.delayStart();
+
+      System.config({
+        paths: {
+          '*': '*'
+        }
+      });
+      for (; i < len; i++) {
+        promises.push(System['import'](wallaby.tests[i].replace(/\.js$/, '')));
+      }
+
+      System.import('test/unit/setup')
+        .then(function () {
+          return Promise.all(promises);
+        })
+        .then(function() {
+          wallaby.start();
+        }).catch(function (e) { setTimeout(function (){ throw e; }, 0); });
+    },
+
+    debug: false
+  };
+};
