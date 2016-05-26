ba622447de3dfe313c80c064fc69713ee8d78101
Matt Kruczek <matthew.kruczek@Tallan.com>
2016-05-26 13:05:15 -0700

    Added two new Angular 2 templates - bootstrap - root component
    Added 9 Typescript Snippets for when they get snippets working in typescript
    - ng2-bootstrap
    - ng2-component
    - ng2-component-root
    - ng2-http-get
    - ng2-pipe
    - ng2-route-path
    - ng2-routes
    - ng2-service
    - ng2-subscribe

---
+++ TemplatePack/ItemTemplates/Web/TypeScript/Angular2/Bootstrap/bootstrap.ts
@@ -0,0 +1,10 @@
+﻿import { enableProdMode } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './name.component';
+
+// enableProdMode();
+
+bootstrap(AppComponent)
+    .then(success => console.log(`Bootstrap success`))
+    .catch(error => console.log(error));
