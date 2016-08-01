2cec9ead68cf5b1dfc02669080c15854b2411e03
Ryan Langton <rlangton76@gmail.com>
2016-08-01 13:54:37 -0500

    added angular 1.5 component template

---
+++ TemplatePack/ItemTemplates/Web/JavaScript/AngularJs/Component/component.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .component('$safeitemname$', $safeitemname$Component);
+
+    $safeitemname$Component.$inject = ['$http']; 
+
+    function $safeitemname$Component($http) {
+        var $ctrl = this;
+
+        $ctrl.$onInit = function(){
+            $ctrl.title = $safeitemname$;
+        }
+    }
+})();
